32c979a18bf39be3343518df8d82e7d90ec169a6
Kagradiel <matheusp_torres@hotmail.com>
2025-01-10 01:08:30 -0300

    responsividade adicionada

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,5 @@
-const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+import { formsValidation } from './formsValidator.js';
+import { tamanhoDaTela } from './menu.js';
 
-formulario.addEventListener('submit', (e) => {
-    
-    e.preventDefault();
-
-    const campoNome = document.querySelector('#name');
-    const txtNome = document.querySelector('#txtNome');
-
-    if (campoNome.value.length < 3) {
-        txtNome.innerHTML = 'Nome deve ter no mínimo 3 caracteres';
-        campoNome.focus();
-        return;
-    }else{
-        txtNome.innerHTML = '';
-    }
-
-    const campoEmail = document.querySelector('#email');
-    const txtEmail = document.querySelector('#txtEmail');
-
-    if (!campoEmail.value.match(emailRegex)) {
-        txtEmail.innerHTML = 'Email inválido';
-        campoEmail.focus();
-        return;
-    }else{
-        txtEmail.innerHTML = '';
-    }
-
-    const campoSubject = document.querySelector('#subject');
-    const txtSubject = document.querySelector('#txtSubject');
-  
-    if (campoSubject.value.length < 5) {
-        txtSubject.innerHTML = 'Assunto deve ter no mínimo 5 caracteres';
-        campoSubject.focus();
-        return;
-    }else{
-        txtSubject.innerHTML = '';
-    }
-
-    const campoMessage = document.querySelector('#message');
-
-    window.alert('Formulário enviado com sucesso!');
-
-    campoNome.value = '';
-    campoEmail.value = '';
-    campoSubject.value = '';
-    campoMessage.value = '';
-    
-    
-});
+tamanhoDaTela();
+formsValidation();
